9907d8d02d3b68ec7b4bc9f28590c89fa93892a1
Benjamin Chalmers <benj@minchalme.rs>
2018-05-14 23:38:29 +0100

    Adding the workings

--- pick.c
+++ pick.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/rand.h>
+#include <openssl/err.h>
+#include <assert.h>
 
 struct person
 {
   char* name;
-  int priority;
+  unsigned long int priority;
+  RSA* key;
 };
 
 char* read_the_line()
@@ -23,13 +29,12 @@ char* read_the_line()
 
 void clear_input_buffer()
 {
-  char i;
-  while((i = fgetc(stdin)) != EOF && i != '\n');
+  char c;
+  while ((c = getchar()) != '\n' && c != EOF);
 }
 
 int main()
 {
-  int seed;
   int num;
   printf("Enter the number of people: ");
   scanf("%i", &num);
@@ -37,18 +42,44 @@ int main()
   struct person* people;
   people = malloc(sizeof(struct person) * num);
   clear_input_buffer();
-  for (int i = 0; i < num; ++i)
+  printf("Enter your name: ");
+  people->name = read_the_line();
+
+  //// RSA GENERATION
+
+  RSA* localKey = NULL;
+  RSA* pact = NULL;
+  BIGNUM* bne = NULL;
+  bne = BN_new();
+  BN_set_word(bne, RSA_F4);
+  people->key = RSA_new();
+
+  printf("Please seed the srng used for the key generation. Keep this hidden\n"
+      ">>> ");
+  char* seed = read_the_line();
+  RAND_seed(seed, strlen(seed));
+
+  if (RSA_generate_key_ex(people->key, 4096, bne, NULL) == 0)
+    printf("%s\n",  ERR_reason_error_string(ERR_get_error()));
+  printf("This is your public key, give it to your friends:\n");
+  PEM_write_RSAPublicKey(stdout, people->key);
+
+  printf("Fill in friends details:\n");
+  for (int i = 1; i < num; ++i)
   {
-    printf("%i) ", i + 1);
-    people[i].name = read_the_line();
+    printf("Name (%i/%i): ", i, num - 1);
+    people[i].name = read_the_line(); 
+    printf("Public Key:\n");
+    PEM_read_RSAPublicKey(stdin, &(people[i].key), NULL, NULL);
   }
 
-  scanf("%i", &seed);
-  srand(seed);
+  for(int i = 0; i < num; ++i)
+  {
+    printf("%i) %s \n", i + 1, people[i].name);
+    PEM_write_RSAPublicKey(stdout, people[i].key);
+  }
 
-  for (int i = 0; i < num; ++i)
-    people[i].priority = rand();
-
+/*
   for (int j = 0; j < num; ++j)
   {
     struct person* max = people;
@@ -56,7 +87,8 @@ int main()
     {
       if (people[i].priority > max->priority) max = people + i;
     }
-    printf("%i) %s %i\n", j + 1, max->name, max->priority);
-    max->priority = -1;
+    printf("%i) %s %lu\n", j + 1, max->name, max->priority);
+    max->priority = 0;
   }
+  */
 }
